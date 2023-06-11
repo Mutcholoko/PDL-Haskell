53b0069abaf4f20db578baf09966fb5043c17870
Lucas Souza <lu_oliveira@id.uff.br>
2023-06-10 23:14:37 -0300

    a versão 2 experimental estreia

--- main.hs
+++ main.hs
@@ -1,21 +1,72 @@
-module Main where
+import Data.Array
 
-data MatrizAdjacencias = MatrizAdjacencias [[String]]
+createMatrix :: Int -> Int -> IO [[[String]]]
+createMatrix 0 _ = return []
+createMatrix numRows numCols = do
+  row <- createRow numCols 1
+  rest <- createMatrix (numRows - 1) numCols
+  return (row : rest)
+  where
+    createRow 0 _ = return []
+    createRow n rowNum = do
+      putStrLn ("Enter element at position (" ++ show rowNum ++ ", " ++ show (numCols - n + 1) ++ "):")
+      val <- getLine
+      rest <- createRow (n - 1) rowNum
+      return (words val : rest)
 
-matrizVazia :: Int -> MatrizAdjacencias
-matrizVazia tam = MatrizAdjacencias (replicate tam (replicate tam "testeAPAGUE"))
+type Graph = Array (Int, Int) [String]
 
-getPrograma :: MatrizAdjacencias -> Int -> Int -> String
-getPrograma (MatrizAdjacencias matriz) linha coluna = (matriz !! linha) !! coluna
+graph :: Int -> IO (Graph)
+graph size = do
+  matrixVals <- createMatrix size size
+  let indexedVals = [((i, j), (matrixVals !! (i-1)) !! (j-1)) | i <- [1..size], j <- [1..size]]
+  return $ array ((1, 1), (size, size)) indexedVals
 
-setPrograma :: MatrizAdjacencias -> Int -> Int -> String -> MatrizAdjacencias
-setPrograma (MatrizAdjacencias matriz) linha coluna programa = 
-    let (before, current:after) = splitAt linha matriz
-        novaLinha = take coluna current ++ [programa] ++ drop (coluna + 1) current in MatrizAdjacencias (before ++ (novaLinha : after))
+type W = (Int, [String])
+
+states :: Int -> IO [W]
+states size
+    | size <= 0 = return []
+    | otherwise = do
+        putStrLn $ "Enter strings for vertex " ++ show size ++ ":"
+        strings <- getLine
+        rest <- states (size - 1)
+        return $ (size, words strings) : rest
+
+data Node = UnaryNode Node Char | BinaryNode Node Node Char | Leaf Char deriving Show
+
+buildExpressionTree :: [Char] -> Node
+buildExpressionTree xs = buildTree xs []
+  where
+    buildTree [] stack = head stack
+    buildTree (x:xs) stack
+      | isUnaryOperator x = do
+          let child = head stack
+          let new_node = UnaryNode child x
+          let updated_stack = tail stack
+          buildTree xs (new_node : updated_stack)
+      | isOperator x = do
+          let right = head stack
+          let left = stack !! 1
+          let new_node = BinaryNode left right x
+          let updated_stack = drop 2 stack
+          buildTree xs (new_node : updated_stack)
+      | otherwise = buildTree xs (Leaf x : stack)
+
+isUnaryOperator :: Char -> Bool
+isUnaryOperator x = x `elem` "?~*"
+
+isOperator :: Char -> Bool
+isOperator x = x `elem` ";>^v"
 
 main :: IO ()
 main = do 
-    putStrLn "Comecando"
-    let matriz = matrizVazia 2
-    putStrLn (getPrograma matriz 1 1)
-    
+    putStrLn "Enter the number of vertices:"
+    vertices <- readLn
+    matrix <- graph vertices
+    putStrLn $ "Created matrix: " ++ show matrix
+    set <- states vertices
+    print set
+    let postfixExpression = "a?ab;?"
+    let root = buildExpressionTree postfixExpression
+    putStrLn $ "Expression Tree: " ++ show root
