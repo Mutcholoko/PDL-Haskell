8e3fca3279b096b5b98d581bbc7dc5b26a01fc90
Gabriel Gavazzi <gabrielgavazzi@id.uff.br>
2023-06-12 16:48:53 -0300

    enfeites de natal

--- main.hs
+++ main.hs
@@ -1,72 +1,72 @@
 import Data.Array
 
-createMatrix :: Int -> Int -> IO [[[String]]]
-createMatrix 0 _ = return []
-createMatrix numRows numCols = do
+criaMatriz :: Int -> Int -> IO [[[String]]]
+criaMatriz 0 _ = return []
+criaMatriz numRows numCols = do
   row <- createRow numCols 1
-  rest <- createMatrix (numRows - 1) numCols
+  rest <- criaMatriz (numRows - 1) numCols
   return (row : rest)
   where
     createRow 0 _ = return []
     createRow n rowNum = do
-      putStrLn ("Enter element at position (" ++ show rowNum ++ ", " ++ show (numCols - n + 1) ++ "):")
+      putStrLn ("Insira o elemento na posicao (" ++ show rowNum ++ ", " ++ show (numCols - n + 1) ++ "):")
       val <- getLine
       rest <- createRow (n - 1) rowNum
       return (words val : rest)
 
-type Graph = Array (Int, Int) [String]
+type Grafo = Array (Int, Int) [String]
 
-graph :: Int -> IO (Graph)
-graph size = do
-  matrixVals <- createMatrix size size
+grafo :: Int -> IO (Grafo)
+grafo size = do
+  matrixVals <- criaMatriz size size
   let indexedVals = [((i, j), (matrixVals !! (i-1)) !! (j-1)) | i <- [1..size], j <- [1..size]]
   return $ array ((1, 1), (size, size)) indexedVals
 
 type W = (Int, [String])
 
-states :: Int -> IO [W]
-states size
+estados :: Int -> IO [W]
+estados size
     | size <= 0 = return []
     | otherwise = do
-        putStrLn $ "Enter strings for vertex " ++ show size ++ ":"
+        putStrLn $ "Insira a string para o vertice " ++ show size ++ ":"
         strings <- getLine
-        rest <- states (size - 1)
+        rest <- estados (size - 1)
         return $ (size, words strings) : rest
 
-data Node = UnaryNode Node Char | BinaryNode Node Node Char | Leaf Char deriving Show
+data Node = NoUnario Node Char | NoBinario Node Node Char | Folha Char deriving Show
 
-buildExpressionTree :: [Char] -> Node
-buildExpressionTree xs = buildTree xs []
+criaArvoreExpressao :: [Char] -> Node
+criaArvoreExpressao xs = criaArvore xs []
   where
-    buildTree [] stack = head stack
-    buildTree (x:xs) stack
-      | isUnaryOperator x = do
+    criaArvore [] stack = head stack
+    criaArvore (x:xs) stack
+      | isOperadorUnario x = do
           let child = head stack
-          let new_node = UnaryNode child x
+          let new_node = NoUnario child x
           let updated_stack = tail stack
-          buildTree xs (new_node : updated_stack)
-      | isOperator x = do
+          criaArvore xs (new_node : updated_stack)
+      | isOperador x = do
           let right = head stack
           let left = stack !! 1
-          let new_node = BinaryNode left right x
+          let new_node = NoBinario left right x
           let updated_stack = drop 2 stack
-          buildTree xs (new_node : updated_stack)
-      | otherwise = buildTree xs (Leaf x : stack)
+          criaArvore xs (new_node : updated_stack)
+      | otherwise = criaArvore xs (Folha x : stack)
 
-isUnaryOperator :: Char -> Bool
-isUnaryOperator x = x `elem` "?~*"
+isOperadorUnario :: Char -> Bool
+isOperadorUnario x = x `elem` "?~*"
 
-isOperator :: Char -> Bool
-isOperator x = x `elem` ";>^v"
+isOperador :: Char -> Bool
+isOperador x = x `elem` ";>^v"
 
 main :: IO ()
 main = do 
-    putStrLn "Enter the number of vertices:"
+    putStrLn "Insira o numero de vertices:"
     vertices <- readLn
-    matrix <- graph vertices
-    putStrLn $ "Created matrix: " ++ show matrix
-    set <- states vertices
+    matriz <- grafo vertices
+    putStrLn $ "Matriz criada: " ++ show matriz
+    set <- estados vertices
     print set
     let postfixExpression = "a?ab;?"
-    let root = buildExpressionTree postfixExpression
-    putStrLn $ "Expression Tree: " ++ show root
+    let raiz = criaArvoreExpressao postfixExpression
+    putStrLn $ "Arvore de Expressao: " ++ show raiz
